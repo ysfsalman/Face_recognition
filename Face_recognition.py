9f019b29291bdc230034b65617dda94dda8176ea
woycoy <yusufsalman@hotmail.co.id>
2020-01-25 09:03:21 +0100

    make code neater

--- Face_recognition.py
+++ Face_recognition.py
@@ -1,22 +1,28 @@
-import cv2
-import numpy as np
+# code to collect initial dataset for face recognition training
 
+import cv2
+
+# get a classifier model
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
-cv2.namedWindow("preview")
+# make capture object
 vc = cv2.VideoCapture(0)
-i = 0
  
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
-
+i = 0
+cv2.namedWindow("Record")
 while rval:
     
+    # get frame
     rval, frame = vc.read()
 
+    # filter frame to gray
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # classify face from grayed image
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.5,
@@ -25,19 +31,23 @@ while rval:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
     
+    # acquire face image dataset and resize to a standard dimension
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
-        name = 'Yusuf/'+str(i)+'.png'
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print(x,y,w,h) # to check suitable standard dimension
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # draw rectangle on the frame
+        
         # dataset resize
         dim = (125,125)
         graysized = cv2.resize(gray[y:y+h,x:x+w],dim,interpolation=cv2.INTER_AREA)
+
+        # store frame
+        name = 'Yusuf/'+str(i)+'.png'
         cv2.imwrite(name, graysized)
         i = i+1
-    cv2.imshow('FaceDetection', frame)    
     
-    
+    cv2.imshow('Record', frame)    
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+
 cv2.destroyWindow("preview")
